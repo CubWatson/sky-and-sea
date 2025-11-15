6d7954f0bc79678dddd7baecebae6bc18b0923a7
CubWatson <ktherston@gmail.com>
2025-11-15 03:58:21 -0600

    refactoring names

--- src/main/java/com/cubwatson/skyandsea/SkyAndSea.java
+++ src/main/java/com/cubwatson/skyandsea/SkyAndSea.java
@@ -32,8 +32,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(SkyandSea.MODID)
-public class SkyandSea {
+@Mod(SkyAndSea.MODID)
+public class SkyAndSea {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "skyandsea";
     // Directly reference a slf4j logger
@@ -65,7 +65,7 @@ public class SkyandSea {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public SkyandSea(IEventBus modEventBus, ModContainer modContainer) {
+    public SkyAndSea(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
